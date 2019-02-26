10cc2596f89fd9d31593ea52471e8aca0d4023be
anwar-kalghoum <anwarkalghoum@oyez.fr>
2019-02-26 14:14:43 +0100

    frr

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import App from './App';
 
+it('Test Snapshot ', () => {
+  const tree = renderer
+  .create(<App />)
+  .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
 });
